272e964dc6e8c0727fe7d1bc5c8831e9c52a3235
Mihail <mihailvgeorgiev00@gmail.com>
2022-04-28 00:03:35 +0300

    changed setting static root

--- projectshop/projectshop/urls.py
+++ projectshop/projectshop/urls.py
@@ -1,7 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projectshop.main.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
