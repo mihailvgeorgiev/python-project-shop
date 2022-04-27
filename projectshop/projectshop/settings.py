219888d00db7d26c3a9e51f15837b9f491a304bd
Mihail <mihailvgeorgiev00@gmail.com>
2022-04-27 23:56:54 +0300

    changed base html

--- projectshop/projectshop/settings.py
+++ projectshop/projectshop/settings.py
@@ -143,7 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR_2, 'static')
+    os.path.join(BASE_DIR_2, 'static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
